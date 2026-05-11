0620183447904053640f34c6280c95382d40ca5b
lexxaxell212 <febryagung404@gmail.com>
2026-05-11 13:41:24 +0700

    update

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,11 @@
 <?php
-require_once __DIR__ . '/maintenance.php';
-//
 // Hello Core //
 $lib_path = dirname(__DIR__) . "/lib/functions.php";
 if (!file_exists($lib_path)) die("lib/functions.php missing: " . $lib_path);
 require_once $lib_path;
 autoload_core(); // Core Here //
+
+require_once __DIR__ . '/maintenance.php';
 
 $page_title = htmlspecialchars($_POST["title"] ?? ($page_title ?? "Ayokebandung.id"));
 

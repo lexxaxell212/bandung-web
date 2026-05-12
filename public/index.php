184296f50b8318d92c193c093435719fdc791adc
lexxaxell212 <febryagung404@gmail.com>
2026-05-13 06:09:39 +0700

    update

--- public/index.php
+++ public/index.php
@@ -3,20 +3,15 @@ require_once __DIR__ . "/../bootstrap.php";
 autoload_core();
 
 require_once __DIR__ . "/router.php";
+
 $page_title = $page_title ?? "Ayokebandung.id";
 
-require_once SRC_PATH . "header.php";
-?>
-
-<?php
 if (isset($view_content) && file_exists($view_content)) {
+    require_once SRC_PATH . "header.php";
     require_once $view_content;
+    require_once SRC_PATH . "footer.php";
 } else {
     http_response_code(404);
-    require_once SRC_PATH . "/errors/404.php";
+    require_once SRC_PATH . "errors/404.php";
 }
-?>
-
-<?php
-require_once SRC_PATH . "footer.php"; 
 ?>

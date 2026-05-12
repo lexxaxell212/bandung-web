052b5dcbac4fe2179a75789a9f6b278582aaae5d
lexxaxell212 <febryagung404@gmail.com>
2026-05-13 03:21:12 +0700

    update

---
+++ errors/404.php
@@ -0,0 +1,8 @@
+<?php
+// 404.php
+http_response_code(404);
+?>
+    <div class="py-5">
+        <h1>404</h1>
+        <p>Halaman yang kamu cari tidak ditemukan.<br>Mungkin sudah dipindah atau dihapus.</p>
+    </div>

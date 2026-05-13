a454be0df0034124248de3aa7e6b874ce870518d
lexxaxell212 <febryagung404@gmail.com>
2026-05-13 09:54:03 +0700

    update

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ autoload_core();
 // Maintenance check
 require_once ROOT_PATH . "maintenance.php";
 // Router
-require_once ROOT_PATH . "public/router.php";
+require_once PUBLIC_PATH . "router.php";
 ////// =============================== //////
 $page_title = $page_title ?? SITE_NAME;
 if (isset($view_content) && file_exists ($view_content)) {
@@ -19,7 +19,7 @@ if (isset($view_content) && file_exists ($view_content)) {
     require_once SRC_PATH . "footer.php";
 } else {
     http_response_code(404);
-    require_once BASE_URL . "/errors/404.php";
+    require_once PUBLIC_PATH . "errors/404.php";
 }
 ////// =============================== //////
 ?>
